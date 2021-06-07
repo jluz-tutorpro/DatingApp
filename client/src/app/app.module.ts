96a5580bfdc77103c8735e9f23c33cc4c4dbfc5b
jluz@tutorpro.com <jluz@tutorpro.com>
2021-06-07 21:25:11 +0100

    End of section 3

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
